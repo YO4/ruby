684365319c5ddf8436ef3efb3a8e362e7186a949
YO4 <ysno@ac.auone-net.jp>
2024-12-06 00:05:10 +0900

    tweak LC_CTYPE on windows ucrt
    Mitigate problems when the user's language settings are different from the system defaults.

--- main.c
+++ main.c
@@ -52,6 +52,10 @@ int rb_wasm_rt_start(int (main)(int argc, char **argv), int argc, char **argv);
 #define main(argc, argv) w32_main(argc, argv)
 static int main(int argc, char **argv);
 int wmain(void) {return main(0, NULL);}
+#if defined(RUBY_MSVCRT_VERSION) && RUBY_MSVCRT_VERSION >= 140
+#define W32_NEED_LOCALE_TWEAK 1
+static void w32_tweak_locale(void);
+#endif
 #endif
 
 int
@@ -62,11 +66,49 @@ main(int argc, char **argv)
 #endif
 #ifdef HAVE_LOCALE_H
     setlocale(LC_CTYPE, "");
+#ifdef W32_NEED_LOCALE_TWEAK
+    w32_tweak_locale();
+#endif
 #endif
 
     ruby_sysinit(&argc, &argv);
     return rb_main(argc, argv);
 }
+
+#ifdef W32_NEED_LOCALE_TWEAK
+/* Windows Universal CRT has inconsistent language / codepage behavior.
+ * If an user ui language setting differs to a system ui language,
+ * setlocale(any, "") combines that user ui language with a system default
+ * code page. This is different from CRTs prior to Universal CRT.
+ * On the other hand, CRT holds time zone name as narrow char string if 
+ * Universal CRT build is less than 10.0.19041.0. 
+ * Time zone names are selected by an user ui language, CRT converts it to
+ * narrow char using locale code page.
+ * This may cause destruction of time zone names.
+ * It seems that a call to setlocale() without a code page will select
+ * the default code page for the specified locale.
+ * This seems to work for CRTs prior to universal CRT. */
+static void
+w32_tweak_locale(void)
+{
+    char *original_lc_ctype = setlocale(LC_CTYPE, NULL);
+
+    if (original_lc_ctype) {
+        char *tweaked_lc_ctype = NULL;
+        char *p;
+
+        tweaked_lc_ctype = alloca(strlen(original_lc_ctype) + 1);
+        for (p = tweaked_lc_ctype; *original_lc_ctype; original_lc_ctype++, p++) {
+            if (*original_lc_ctype == '.') {
+                *p = '\0';
+                break;
+            }
+            *p = *original_lc_ctype;
+        }
+        setlocale(LC_CTYPE, tweaked_lc_ctype);
+    }
+}
+#endif
 
 #ifdef RUBY_ASAN_ENABLED
 /* Compile in the ASAN options Ruby needs, rather than relying on environment variables, so
