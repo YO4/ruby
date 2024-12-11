893e98f0356fc29578bde632b6ba7c0ea61bfd77
YO4 <ysno@ac.auone-net.jp>
2024-12-11 20:27:13 +0900

    fix incompatible language and code page combination of setlocale()

--- main.c
+++ main.c
@@ -52,6 +52,14 @@ int rb_wasm_rt_start(int (main)(int argc, char **argv), int argc, char **argv);
 #define main(argc, argv) w32_main(argc, argv)
 static int main(int argc, char **argv);
 int wmain(void) {return main(0, NULL);}
+#if defined(RUBY_MSVCRT_VERSION) && RUBY_MSVCRT_VERSION >= 140
+#define W32_NEED_LOCALE_TWEAK 1
+static void w32_tweak_locale(void);
+#define W32_LOCALE_TWEAK() w32_tweak_locale()
+#endif
+#endif
+#ifndef W32_LOCALE_TWEAK
+#define W32_LOCALE_TWEAK() (0)
 #endif
 
 int
@@ -62,11 +70,47 @@ main(int argc, char **argv)
 #endif
 #ifdef HAVE_LOCALE_H
     setlocale(LC_CTYPE, "");
+    W32_LOCALE_TWEAK();
 #endif
 
     ruby_sysinit(&argc, &argv);
     return rb_main(argc, argv);
 }
+
+#ifdef W32_NEED_LOCALE_TWEAK
+static void
+w32_tweak_locale(void)
+{
+    char *original_lc_ctype = setlocale(LC_CTYPE, NULL);
+
+    if (original_lc_ctype && strchr(original_lc_ctype, '.')) {
+        char *p, *q;
+        char *tweaked_lc_ctype;
+
+        p = original_lc_ctype;
+        q = tweaked_lc_ctype = alloca(strlen(original_lc_ctype) + 1);
+        while (*p != '.') {
+            *q++ = *p++;
+        }
+        /* return if locale codepage == UTF-8 */
+        if (strlen(p) >= 5) {
+            if (TOLOWER(p[1]) == 'u' &&
+                TOLOWER(p[2]) == 't' &&
+                TOLOWER(p[3]) == 'f' &&
+                ((p[4] == '8' && p[5] == '\0') ||
+                 (p[4] == '-' && p[5] == '8' && p[6] == '\0'))) {
+                return;
+            }
+        }
+        *q = '\0';
+        /* Set locale codepage to locale language's default ANSI codepage.
+         * This avoids different language-codepage combination
+         * like "Japanese_Japan.1252". Setting Default User Interface Language
+         * to different from Windows System Default Language causes this. */
+        setlocale(LC_CTYPE, tweaked_lc_ctype);
+    }
+}
+#endif
 
 #ifdef RUBY_ASAN_ENABLED
 /* Compile in the ASAN options Ruby needs, rather than relying on environment variables, so
