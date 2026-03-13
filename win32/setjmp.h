592c81f008fde3753443b7db7a080f02916b443f
YO4 <ysno@ac.auone-net.jp>
2026-03-13 22:18:33 +0900

    win32: for MSVC arm64, using setjmp without SEH is not simple

--- win32/setjmp.h
+++ win32/setjmp.h
@@ -2,8 +2,13 @@
 #define RUBY_WIN32_SETJMP_H
 
 #if defined(__MINGW32__) && defined(_WIN64) && defined(RUBY_JMP_BUF)
+/* note: __builtin_setjmp is used unless RUBY_JMP_BUF */
+#if __MINGW64_VERSION_MAJOR >= 11
 /* mingw64 clang or gcc without __builtin_setjmp */
+/* mingw-w64 v11.0.0 feature */
 #define __USE_MINGW_SETJMP_NON_SEH
+#define RUBY_SETJMP_NON_SEH 1
+#endif
 #endif
 
 #include <setjmp.h>
@@ -12,14 +17,15 @@
 /* msvc */
 #if defined(_M_AMD64)
 #define rb_w32_setjmp_func __intrinsic_setjmp
+#define RUBY_SETJMP_NON_SEH 1
 #elif defined(_M_ARM64)
-#define rb_w32_setjmp_func __intrinsic_setjmpex
+/* does not work */
+/* #define rb_w32_setjmp_func __intrinsic_setjmpex */
 #endif
 
 #if defined(rb_w32_setjmp_func)
 /* Include it beforehand to avoid the side effects of #define. */
 #include <intrin.h>
-
 
 /* Declare setjmp with hidden arguments */
 extern int rb_w32_setjmp_func(jmp_buf, void *);
@@ -29,6 +35,9 @@ extern int rb_w32_setjmp_func(jmp_buf, void *);
  */
 #define rb_w32_setjmp(env) rb_w32_setjmp_func((env), NULL)
 
+#else
+/* simple _setjmp call using SEH */
+#define rb_w32_setjmp(env) _setjmp(env)
 #endif /* rb_w32_setjmp_func */
 #endif /* _MSC_VER */
 
