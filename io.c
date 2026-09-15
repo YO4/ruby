0615061afc4e7a96cd8a9c948e6471e50da8b58c
YO4 <ysno@ac.auone-net.jp>
2026-09-15 10:54:33 +0900

    io: skip fd wait in rb_io_getc when converted chars are buffered

--- io.c
+++ io.c
@@ -5683,7 +5683,9 @@ rb_io_getc(VALUE io)
     rb_io_check_char_readable(fptr);
 
     enc = io_input_encoding(fptr);
-    READ_CHECK(fptr);
+    if (!READ_CHAR_PENDING(fptr)) {
+        READ_CHECK(fptr);
+    }
     return io_getc(fptr, enc);
 }
 
